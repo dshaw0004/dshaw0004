eb644e851a5bef2593b0082e6a1116160759db69
dshaw0004 <dipankarshaw692@gmail.com>
2023-01-01 13:10:19 +0530

    first commit

---
+++ src/Pages/Landing/LandingAnimation.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./landinganimation.css";
+
+export default function LandingAnimation() {
+	return (
+		<div id="landing-animation">
+			<h1>Hello, I am Dipankar.</h1>
+			<p class="subtitle">Welcome to my website!</p>
+		</div>
+	);
+}
